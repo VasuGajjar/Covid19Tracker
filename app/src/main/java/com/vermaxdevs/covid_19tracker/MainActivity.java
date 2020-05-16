6f0bde5c52460ba118bb5c1ada8fd69a4d9fc779
vasug <vasugajjar1@gmail.com>
2020-05-16 23:43:09 +0530

    Added Global Option and Last Updated Time

--- app/src/main/java/com/vermaxdevs/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/vermaxdevs/covid_19tracker/MainActivity.java
@@ -19,7 +19,9 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -56,12 +58,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(position == 0) {
-                    new India().execute();
+                    new Global().execute();
                 }
                 else if (position == 1) {
-                    new Gujarat().execute();
+                    new India().execute();
                 }
                 else if (position == 2) {
+                    new Gujarat().execute();
+                }
+                else if (position == 3) {
                     new Ahmedabad().execute();
                 }
             }
@@ -71,6 +76,152 @@ public class MainActivity extends AppCompatActivity {
                 new India().execute();
             }
         });
+    }
+
+    private String timeAgo(String dataDate) {
+        String convTime = null;
+
+        String prefix = "";
+        String suffix = "Ago";
+
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Date pasTime = dateFormat.parse(dataDate);
+
+            Date nowTime = new Date();
+
+            long dateDiff = nowTime.getTime() - pasTime.getTime();
+
+            long second = TimeUnit.MILLISECONDS.toSeconds(dateDiff);
+            long minute = TimeUnit.MILLISECONDS.toMinutes(dateDiff);
+            long hour   = TimeUnit.MILLISECONDS.toHours(dateDiff);
+            long day  = TimeUnit.MILLISECONDS.toDays(dateDiff);
+
+            if (second < 60) {
+                convTime = second+" Seconds "+suffix;
+            } else if (minute < 60) {
+                convTime = minute+" Minutes "+suffix;
+            } else if (hour < 24) {
+                convTime = hour+" Hours "+suffix;
+            } else if (day >= 7) {
+                if (day > 360) {
+                    convTime = (day / 30) + " Years " + suffix;
+                } else if (day > 30) {
+                    convTime = (day / 360) + " Months " + suffix;
+                } else {
+                    convTime = (day / 7) + " Week " + suffix;
+                }
+            } else if (day < 7) {
+                convTime = day+" Days "+suffix;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("Covid Tracker", e.getMessage());
+        }
+
+        return convTime;
+    }
+
+    private String timeAgo2(String dataDate) {
+        String convTime = null;
+
+        String prefix = "";
+        String suffix = "Ago";
+
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date pasTime = dateFormat.parse(dataDate);
+
+            Date nowTime = new Date();
+
+            long dateDiff = nowTime.getTime() - pasTime.getTime();
+
+            long second = TimeUnit.MILLISECONDS.toSeconds(dateDiff);
+            long minute = TimeUnit.MILLISECONDS.toMinutes(dateDiff);
+            long hour   = TimeUnit.MILLISECONDS.toHours(dateDiff);
+            long day  = TimeUnit.MILLISECONDS.toDays(dateDiff);
+
+            if (second < 60) {
+                convTime = second+" Seconds "+suffix;
+            } else if (minute < 60) {
+                convTime = minute+" Minutes "+suffix;
+            } else if (hour < 24) {
+                convTime = hour+" Hours "+suffix;
+            } else if (day >= 7) {
+                if (day > 360) {
+                    convTime = (day / 30) + " Years " + suffix;
+                } else if (day > 30) {
+                    convTime = (day / 360) + " Months " + suffix;
+                } else {
+                    convTime = (day / 7) + " Week " + suffix;
+                }
+            } else if (day < 7) {
+                convTime = day+" Days "+suffix;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("Covid Tracker", e.getMessage());
+        }
+
+        return convTime;
+    }
+
+    class Global extends AsyncTask<Void, Void, JSONObject> {
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Please Wait");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            super.onPostExecute(jsonObject);progressDialog.dismiss();
+            progressDialog.dismiss();
+
+            try {
+                updateTime.setText(jsonObject.getJSONObject("world_total").toString());
+                todayCase.setText(jsonObject.getJSONObject("world_total").getString("new_cases"));
+                todayRecovered.setText("NA");
+                todayDeath.setText(jsonObject.getJSONObject("world_total").getString("new_deaths"));
+                totalCase.setText(jsonObject.getJSONObject("world_total").getString("total_cases"));
+                totalRecovered.setText(jsonObject.getJSONObject("world_total").getString("total_recovered"));
+                totalDeath.setText(jsonObject.getJSONObject("world_total").getString("total_deaths"));
+                active.setText(jsonObject.getJSONObject("world_total").getString("active_cases"));
+                addCase.setText("+"+jsonObject.getJSONObject("world_total").getString("new_cases"));
+                addDeath.setText("+"+jsonObject.getJSONObject("world_total").getString("new_deaths"));
+                addRecovered.setText("+NA");
+                updateTime.setText("Last Updated "+timeAgo2(jsonObject.getJSONObject("world_total").getString("statistic_taken_at")));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        protected JSONObject doInBackground(Void... voids) {
+            JSONObject object = null;
+
+            try {
+                HttpResponse<String> response = Unirest.get("https://corona-virus-world-and-india-data.p.rapidapi.com/api")
+                        .header("x-rapidapi-host", "corona-virus-world-and-india-data.p.rapidapi.com")
+                        .header("x-rapidapi-key", "f8f40d658cmsh0ec9c8db1e09aacp168ea7jsn1d3fea5ba18e")
+                        .asString();
+
+                object = new JSONObject(String.valueOf(response.getBody()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return object;
+        }
     }
 
     class India extends AsyncTask<Void, Void, JSONObject> {
@@ -102,7 +253,7 @@ public class MainActivity extends AppCompatActivity {
                 addCase.setText("+"+jsonObject.getJSONObject("total_values").getString("deltaconfirmed"));
                 addDeath.setText("+"+jsonObject.getJSONObject("total_values").getString("deltadeaths"));
                 addRecovered.setText("+"+jsonObject.getJSONObject("total_values").getString("deltarecovered"));
-                updateTime.setText("Last Updated "+jsonObject.getJSONObject("total_values").getString("lastupdatedtime"));
+                updateTime.setText("Last Updated "+timeAgo(jsonObject.getJSONObject("total_values").getString("lastupdatedtime")));
             }
             catch (Exception e) {
                 e.printStackTrace();
@@ -158,7 +309,7 @@ public class MainActivity extends AppCompatActivity {
                 addCase.setText("+"+jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getString("deltaconfirmed"));
                 addDeath.setText("+"+jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getString("deltadeaths"));
                 addRecovered.setText("+"+jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getString("deltarecovered"));
-                updateTime.setText("Last Updated "+jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getString("lastupdatedtime"));
+                updateTime.setText("Last Updated "+timeAgo(jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getString("lastupdatedtime")));
             }
             catch (Exception e) {
                 e.printStackTrace();
@@ -213,7 +364,7 @@ public class MainActivity extends AppCompatActivity {
                 addCase.setText("+"+jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getJSONObject("district").getJSONObject("Ahmedabad").getJSONObject("delta").getString("confirmed"));
                 addDeath.setText("+"+jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getJSONObject("district").getJSONObject("Ahmedabad").getJSONObject("delta").getString("deceased"));
                 addRecovered.setText("+"+jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getJSONObject("district").getJSONObject("Ahmedabad").getJSONObject("delta").getString("recovered"));
-                updateTime.setText("Last Updated "+jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getString("lastupdatedtime"));
+                updateTime.setText("Last Updated "+timeAgo(jsonObject.getJSONObject("state_wise").getJSONObject("Gujarat").getString("lastupdatedtime")));
             }
             catch (Exception e) {
                 Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
